e6e5eef8d1582223062c9f211d3a975a55088511
mumbereh <siirasonh@gmail.com>
2023-07-10 23:43:19 +0300

    modified index

--- index.js
+++ index.js
@@ -1,58 +1,57 @@
 function addBook() {
-    const titleInput = document.getElementById('titleInput');
-    const authorInput = document.getElementById('authorInput');
-    const title = titleInput.value.trim();
-    const author = authorInput.value.trim();
-  
-    // Check if title or author is empty
-    if (title === '' || author === '') {
-      window.alert('Please enter both a title and an author.');
-      return; // Stop execution
-    }
-  
-    const bookElement = document.createElement('div');
-  
-    const titleParagraph = document.createElement('p');
-    titleParagraph.innerText = `Title: ${title}`;
-    bookElement.appendChild(titleParagraph);
-  
-    const authorParagraph = document.createElement('p');
-    authorParagraph.innerText = `Author: ${author}`;
-    bookElement.appendChild(authorParagraph);
-  
-    const removeButton = document.createElement('button');
-    removeButton.innerText = 'Remove';
-    removeButton.addEventListener('click', () => {
-      bookElement.remove();
-      removeBookFromLocalStorage(title, author); // Remove book from local storage
-    });
-  
-    bookElement.appendChild(document.createElement('br')); // Add line break
-    bookElement.appendChild(removeButton);
-  
-    document.getElementById('bookList').appendChild(bookElement);
-  
-    // Store author and title in local storage
-    const book = {
-      title,
-      author
-    };
-    let books = JSON.parse(localStorage.getItem('books')) || [];
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-  
-    // Clear input fields
-    titleInput.value = '';
-    authorInput.value = '';
+  const titleInput = document.getElementById('titleInput');
+  const authorInput = document.getElementById('authorInput');
+  const title = titleInput.value.trim();
+  const author = authorInput.value.trim();
+
+  // Check if title or author is empty
+  if (title === '' || author === '') {
+    return; // Stop execution
   }
-  
+
+  const bookElement = document.createElement('div');
+
+  const titleParagraph = document.createElement('p');
+  titleParagraph.innerText = `Title: ${title}`;
+  bookElement.appendChild(titleParagraph);
+
+  const authorParagraph = document.createElement('p');
+  authorParagraph.innerText = `Author: ${author}`;
+  bookElement.appendChild(authorParagraph);
+
+  const removeButton = document.createElement('button');
+  removeButton.innerText = 'Remove';
+  removeButton.addEventListener('click', () => {
+  bookElement.remove();
   // Remove book from local storage
-  function removeBookFromLocalStorage(title, author) {
-    let books = JSON.parse(localStorage.getItem('books')) || [];
-    books = books.filter(book => book.title !== title || book.author !== author);
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-  
-  // Call the addBook function somewhere in your code if it is intended to be used
-  addBook();
-  
+    removeBookFromLocalStorage(title, author); 
+  });
+  // Add line break
+  bookElement.appendChild(document.createElement('br')); 
+  bookElement.appendChild(removeButton);
+
+  document.getElementById('bookList').appendChild(bookElement);
+
+  // Store author and title in local storage
+  const book = {
+    title,
+    author,
+  };
+  const books = JSON.parse(localStorage.getItem('books')) || [];
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
+
+  // Clear input fields
+  titleInput.value = '';
+  authorInput.value = '';
+}
+
+// Remove book from local storage
+function removeBookFromLocalStorage(title, author) {
+  let books = JSON.parse(localStorage.getItem('books')) || [];
+  books = books.filter((book) => book.title !== title || book.author !== author);
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+// Call the addBook function somewhere in your code if it is intended to be used
+addBook();
